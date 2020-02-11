3ea9eb8c4dd158cfa2d4abfe65959145d16bee0f
zhongvei <zhongvei@gmail.com>
2020-02-11 13:56:19 +0800

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Container, Row, Col, Form, FormGroup, Input } from 'reactstrap'
+import Socket from './utilities/Socket'
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState('')
+  const [username, SetUsername] = useState('')
+  const [onlineUsers, SetOnlineUsers] = useState([])
+  const [chat, setChat] = useState([])
+
+  const inputBox = (e) => {
+    setMessage(e.target.value)
+  }
+
+  useEffect(() => {
+    Socket.emit('NEW_USER')
+
+    Socket.on("GET_CURRENT_USER", user => {
+      SetUsername(user.username)
+    })
+
+    Socket.on('UPDATE_USER_LIST', users => {
+      SetOnlineUsers(users)
+    })
+
+  }, [])
+
+  const addNewMessage = e => {
+    e.preventDefault()
+    if (message === '') {
+      alert('Enter something!')
+      setMessage('')
+    } else {
+      const timeNow = Date.now()
+      let data = {
+        username: username,
+        message: message,
+        timestamp: timeNow
+      }
+      Socket.emit('BROADCAST_MESSAGE', data)
+      setMessage('');
+    }
+  }
+
+  Socket.on('RECEIVE_BROADCAST', fromChat => {
+    setChat([...chat, fromChat])
+
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Container fluid>
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }}>
+            <h1>Next Chatroom</h1><hr className="line" />
+          </Col>
+        </Row>
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }}>
+            <h3>Online users</h3>
+            {onlineUsers.map((onlineUser, index) => {
+              return (
+                <h5 key={index}>{onlineUser.username}</h5>
+              )
+            })}
+            <hr className="line" />
+          </Col>
+        </Row>
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }}>
+            <h3>Message Board</h3>
+            {chat.map((log, index) => {
+              return (
+                <div className="messageContainer" key={index}>
+                  <h2>{log.username}</h2>
+                  <h3>{log.message}</h3>
+                  <hr className="line" />
+                  <h6>{Date(log.timestamp)}</h6>
+                </div>
+              )
+            })}
+            <hr className="line" />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }}>
+            <Form onSubmit={addNewMessage}>
+              <FormGroup>
+                <Input type="textarea" name="text" id="exampleText" onChange={inputBox} value={message} />
+                <button className="btn btn-secondary">Add Message</button>
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+
+    </>
+  )
 }
 
 export default App;
---
+++ src/utilities/Socket.js
@@ -0,0 +1,4 @@
+import io from 'socket.io-client'
+const Socket = io('https://lirenyeo-react-group-chat-socket-io-server-1.glitch.me/')
+
+export default Socket
